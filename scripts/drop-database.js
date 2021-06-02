ec063f7b0498f5660299e347395a39db7de1786f
Richard Pentecost <richard.pentecost@madetech.com>
2021-06-02 11:26:59 +0100

    create and drop database scripts created, basic get endpoint created in app.js

---
+++ scripts/drop-database.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql2/promise');
+const path = require('path');
+
+require('dotenv').config({
+  path: path.join(__dirname, '../.env.test'),
+});
+
+const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT} = process.env;
+
+const dropDatabase = async () => {
+  try {
+    const db = await mysql.createConnection({
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      port: DB_PORT,
+    });
+    
+    await db.query(`DROP DATABASE ${DB_NAME}`);
+    db.end();
+  } catch (err) {
+    console.log('ERROR:', err);
+  }
+};
+
+dropDatabase();
--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  console.log('Hitting this endpoint');
+  res.status(200).json({ result: "Hello World" });
+});
 
 module.exports = app;
