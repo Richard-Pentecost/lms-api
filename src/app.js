75f52d52c6ed344a896b66d7d7943f5d9350b8c8
Richard Pentecost <richard.pentecost@madetech.com>
2021-06-07 09:25:04 +0100

    add cors to app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const userRouter = require('./routes/user');
 const farmRouter = require('./routes/farm');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 // app.use('/login', authRouter);
 app.use('/farms', farmRouter);
 app.use('/users', userRouter);
