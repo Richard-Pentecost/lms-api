ade3ca65bd7874bbfd72ab550bb77c6a2e832a21
Richard Pentecost <richard.pentecost@madetech.com>
2022-02-27 10:43:58 +0000

    add functionality to edit farm products

--- src/controllers/farms/updateFarmByUuid.js
+++ src/controllers/farms/updateFarmByUuid.js
@@ -8,70 +8,54 @@ const updateFarmByUuid = async (req, res) => {
 
   try {
 
-    // const foundFarm = await Farm.scope('withId').fetchFarmByUuid(uuid);
+    if (!products) {
+      return res.status(401).json({ error: 'A farm must have products' });
+    }; 
 
-    // if (!foundFarm) {
-    //   return res.status(401).json({ error: 'The farm could not be found' });
-    // }
-    
-    // const existingAssociations = await FarmProduct.fetchAssociationsByFarmId(foundFarm.id);
-    
-    // await Promise.all(products.map(async itemUuid => {
-    //   const product = await Product.scope('withId').fetchProductByUuid(itemUuid);
+    const foundFarm = await Farm.scope('withId').fetchFarmByUuid(uuid);
 
-    //   const checkExistingAssociations = existingAssociations.find(existingAssociation => {
-    //     return existingAssociation.productId === product.id && existingAssociation.farmId === foundFarm.id;
-    //   });
-
-    //   if (!checkExistingAssociations) {
-    //     const association = {
-    //       farmId: foundFarm.id,
-    //       productId: product.id,
-    //     };
-  
-    //     await FarmProduct.create(association);
-    //   }
-    // }));
-
-    // Check to see if an existing association needs to be delete
-    // const productArrWithId = await Promise.all(products.map(async itemUuid => {
-    //   return await Product.scope('withId').fetchProductByUuid(itemUuid);
-    // }));
-
-    // await Promise.all(existingAssociations.map(async association => {
-
-    //   const exists = productArrWithId.find(async product => {
-    //     console.log(association.productId);
-    //     console.log(product.id);
-    //     console.log(association.farmId);
-    //     console.log(foundFarm.id);
-    //     const bool = association.productId === product.id && association.farmId === foundFarm.id;
-    //     console.log(bool);
-    //     return bool;
-    //   });
-      // const exists = products.find(async itemUuid => {
-      //   const product = await Product.scope('withId').fetchProductByUuid(itemUuid);
-      //   console.log(product);
-      //   console.log(association);
-      //   return association.productId === product.id && association.farmId === foundFarm.id;
-      // });
-
-    //   console.log("**********");
-    //   console.log(exists);
-    //   if (!exists) {
-    //     await FarmProduct.destroy({ where: { id: association.id } });
-    //   }
-    // }));
-
-    const [ updatedRows ] = await Farm.update(farm, { where: { uuid } });
-
-    if (updatedRows > 0) {
-      res.sendStatus(201);
-    } else {
-      res.status(401).json({ error: 'The farm could not be found' });
+    if (!foundFarm) {
+      return res.status(401).json({ error: 'The farm could not be found' });
     }
+  
+    const existingAssociations = await FarmProduct.fetchAssociationsByFarmId(foundFarm.id);
+    
+    await Promise.all(products.map(async itemUuid => {
+      const product = await Product.scope('withId').fetchProductByUuid(itemUuid);
+
+      if (product) {
+        const checkExistingAssociations = existingAssociations.find(existingAssociation => {
+          return existingAssociation.productId === product.id && existingAssociation.farmId === foundFarm.id;
+        });
+  
+        if (!checkExistingAssociations) {
+          const association = {
+            farmId: foundFarm.id,
+            productId: product.id,
+          };
+    
+          await FarmProduct.create(association);
+        }
+      }
+    }));
+
+    const productsArrWithId = await Promise.all(products.map(async itemUuid => {
+      const product = await Product.scope('withId').fetchProductByUuid(itemUuid);
+      return !!product && product;
+    }));
+
+    const productsForDeleting = existingAssociations.filter(association => {
+      return !productsArrWithId.find(product => product.id === association.productId) && association;
+    });
+
+    productsForDeleting.forEach(async product => {
+      await FarmProduct.destroy({ where: { id: product.id } });
+    })
+
+    await Farm.update(farm, { where: { uuid } });
+    res.sendStatus(201);
   } catch (error) {
-    console.error(error);
+    // console.error(error);
     res.status(500).json({ error: 'There was an error connecting to the database' });
   }
 };
