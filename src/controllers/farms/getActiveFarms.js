ebe557d669a4c207d8fe75791666fd66708b5c1c
Richard Pentecost <richard.pentecost@madetech.com>
2022-05-10 19:01:48 +0100

    add sort and filter to the getActiveFarms controller and the endpoint on the model

--- src/controllers/farms/getActiveFarms.js
+++ src/controllers/farms/getActiveFarms.js
@@ -2,12 +2,20 @@ const { Farm } = require('../../models');
 
 const getActiveFarms = async (req, res) => {
   try {
-    const searchString = req.query.query ? req.query.query : '';
-  
-    const farms = await Farm.fetchActiveFarms(searchString);
+    const search = req.query.query ? req.query.query : '';
+    const sort = req.query.sort && req.query.sort === 'z-a' ? 'desc' : 'asc';
+
+    let filter = []; 
+
+    if (req.query.filter) {
+      filter = req.query.filter && req.query.filter.includes(',') ?
+        req.query.filter.split(',') : [req.query.filter];
+    }
+    
+    const farms = await Farm.fetchActiveFarms(sort, search, filter);
     res.status(201).json(farms);
   } catch (error) {
-    // console.error(error);
+    console.error(error);
     res.status(500).json({ error: 'There was an error connecting to the database' });
   }
 }
